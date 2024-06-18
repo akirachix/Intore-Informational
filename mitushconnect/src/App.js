201e4601ef67c48eeef520209eb1595985f94e6e
Raziah <abdullahiraziah@gmail.com>
2024-06-18 06:25:02 +0300

    did the mission statement

--- mitushconnect/src/App.js
+++ mitushconnect/src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import LandingPage from "./LandingPage"
+import About from "./About"
+import Team from "./Team"
+import Mission from "./Mission"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () =>{
+  return(
+    <div>
+      <LandingPage/>
+      <About/>
+      <Team/>
+      <Mission/>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
--- mitushconnect/src/index.js
+++ mitushconnect/src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Mission = () =>{
+    return(
+        <div id='main'>
+
+         <h1 className="header">Our Mission</h1>
+        
+         <p className="Mission">To recover waste for its highest and best use while balancing ratesto transform 
+              our business from burying waste to utilizing waste as a resource in order to eliminate the need for landfills.</p>
+
+
+         
+         
+            
+        </div>
+
+
+
+       
+
+        
+
+          
+    )
+}
+export default Mission;
