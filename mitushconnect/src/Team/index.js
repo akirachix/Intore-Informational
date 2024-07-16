2c36aa52c107a81bc4a1c6d9af8c7722f092f805
Mercy-Chemtai <158445902+Mercy-Chemtai@users.noreply.github.com>
2024-07-16 14:20:06 +0300

    Update index.js

--- mitushconnect/src/Team/index.js
+++ mitushconnect/src/Team/index.js
@@ -16,22 +16,22 @@ const Team= ()=>{
             <img src="/Images/Aline.jpg" alt="Aline"/>
             <div className='figure'>
  
-            <p className="software" > Aline Mutesi </p>
-            <p className="software">Software Developer</p>
+            <p className="caption" > Aline Mutesi </p>
+            <p className="caption">Software Developer</p>
             </div>
             </div>
             <div>
             <img src="/Images/Chemtai.jpeg" alt="Chemtai" className='Chemtai'  />
             <div className='figure'>
-            <p className="software"> Mercy Chemtai</p>
-            <p className="software">Software Developer</p>
+            <p className="caption"> Mercy Chemtai</p>
+            <p className="caption">Software Developer</p>
             </div>
             </div>
             <div>
             <img src="/Images/Faith.jpeg" alt="Faith" className='Faith'/>
             <div className='figure'>
-            <p className="software"> Faith Wachira</p>
-            <p className="software">Software Developer</p>
+            <p className="caption"> Faith Wachira</p>
+            <p className="caption">Software Developer</p>
             </div>
             </div>
             </div>
@@ -65,15 +65,15 @@ const Team= ()=>{
             <img src="/Images/Patricia.jpeg" alt="Patricia" className='patricia'/>
             <div className='figure'>
  
-            <p className="software"> Patricia Ainembabazi</p>
-            <p className="software">Software Developer</p>
+            <p className="caption"> Patricia Ainembabazi</p>
+            <p className="caption">Software Developer</p>
             </div>
            </div>
            <div>
             <img src="/Images/Raziah.jpeg" alt="Raziah"  className='raziah'/>
             <div className='figure'>
-            <p className="software"> Raziah Abdullahi</p>
-            <p className="software">Software Developer</p>
+            <p className="caption"> Raziah Abdullahi</p>
+            <p className="caption">Software Developer</p>
             </div>
             </div>
             </div>
