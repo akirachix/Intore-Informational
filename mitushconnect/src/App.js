c9caeafee6c611f14ef9e1a3e2e931b162b13477
student <chemtailodite@gmail.com>
2024-07-16 11:42:07 +0300

    added team feature

---
+++ mitushconnect/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Services from "./OurServices"
+import Team from './Team';
+
+const App = () =>{
+  return(
+    <div>
+        <Services/>
+        <Team/>
+    </div>
+  )
+}
+export default App;
+
