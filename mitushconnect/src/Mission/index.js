87d8bfdc58954491274f1166b7ac15ed28f0c234
Raziah Abdullahi Mohamed <158445801+raziahabdull@users.noreply.github.com>
2024-07-17 11:22:44 +0300

    Update index.js

--- mitushconnect/src/Mission/index.js
+++ mitushconnect/src/Mission/index.js
@@ -7,7 +7,7 @@ const Mission = () =>{
 
        
         <div className="mission">
-            <h2 className='our'>Our Mission</h2>
+            <h2 className='mission-heading'>Our Mission</h2>
             <p className="mission-text"> To recover waste for its highest and best use while balancing rates services.To transform
               our business from burying waste to utilizing waste as a resource.In order to eliminate 
               the need for landfills.</p>
