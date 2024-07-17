da8bd6a7ce6d2c5cd978f082f2a6b61b78e54c37
aine-mbabazi <patainembabazi@gmail.com>
2024-07-17 13:02:39 +0300

    Added about

--- mitushconnect/src/About Us/index.js
+++ mitushconnect/src/About Us/index.js
@@ -3,23 +3,26 @@ const About = ()=>{
     return(
                 
         <div>   
-          <h1 className='about'>About Us</h1>
+          <h1 className='about-heading'>About Us</h1>
           
-          <div className='aboutdiv'>
-            <div className='aboutUs'>
+          <div className='about-container'>
+            {/* <div className='aboutUs'> */}
            {/* <h1 className='aboutHeading'>Mitush
            <br/>Connect</h1> */}
-           <p className="abouttext"> 
-            MitushConnect believes in creating and<br></br> enabling sustainable partnerships
-           
-            to  <br></br> between mitumba traders and 
-            recycling companies  <br></br>to reduce textile waste in Kenya.
-            <br></br>Our goal is to empower Mitumba traders
-            through<br></br>impactful partnerships with recyclers.
+           <div className="about-text">
+           <p > 
+            MitushConnect believes in creating and enabling sustainable partnerships
+            between mitumba traders and 
+            recycling companies to reduce textile waste in Kenya.
+            Our goal is to empower Mitumba traders
+            through impactful partnerships with recyclers.
             </p>
-           </div>
 
-           <div >
+           </div>
+           
+           {/* </div> */}
+
+           <div  className = "image" >
             <img className='image' src="/Images/about2.png" alt='about2'/>
            </div>
 
