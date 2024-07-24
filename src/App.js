2c4ab76a1d1f71df1dc02f778091f9940dd01e54
FaithHope <wachiraafaith>
2024-07-24 15:25:07 +0300

    Add files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Services from "./OurServices"
+
+const App = () =>{
+  return(
+    <div>
+        <Services/>
+    </div>
+  )
+}
+// import App from "./Team";
+export default App;
+
